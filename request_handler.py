bcf41a0f03fd52a443294784bec583120cfd8d04
KyleSimmonsC44 <darrensimmons92@gmail.com>
2021-01-15 14:33:52 -0600

    completed thru chapter 5

--- request_handler.py
+++ request_handler.py
@@ -1,9 +1,9 @@
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from animals import get_all_animals, get_single_animal, create_animal
-from locations import get_all_locations, get_single_location, create_location
-from employees import get_all_employees, get_single_employee, create_employee
-from customers import get_all_customers, get_single_customer, create_customer
+from animals import get_all_animals, get_single_animal, create_animal, delete_animal
+from locations import get_all_locations, get_single_location, create_location, delete_location
+from employees import get_all_employees, get_single_employee, create_employee, delete_employee
+from customers import get_all_customers, get_single_customer, create_customer, delete_customer
 
 # Here's a class. It inherits from another class.
 # For now, think of a class as a container for functions that
@@ -117,9 +117,27 @@ class HandleRequests(BaseHTTPRequestHandler):
         # Encode the new animal and send in response
         self.wfile.write(f"{new_animal}".encode())
 
+    def do_DELETE(self):
+        # Set a 204 response code
+        self._set_headers(204)
 
-    # Here's a method on the class that overrides the parent's method.
-    # It handles any PUT request.
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Delete a single animal from the list
+        if resource == "animals":
+            delete_animal(id)
+        elif resource == "locations":
+            delete_location(id)
+        elif resource == "employees":
+            delete_employee(id)
+        elif resource == "customers":
+            delete_customer(id)
+
+    # Encode the new animal and send in response
+        self.wfile.write("".encode())
+        # Here's a method on the class that overrides the parent's method.
+        # It handles any PUT request.
     def do_PUT(self):
         self.do_POST()
 
