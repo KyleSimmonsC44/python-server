5458bd1bac306ca0f8892099d476322d5d42dc80
KyleSimmonsC44 <darrensimmons92@gmail.com>
2021-01-22 14:28:47 -0600

    completed thru chapter 14

--- kennel.sql
+++ kennel.sql
@@ -57,12 +57,20 @@ INSERT INTO `Animal` VALUES (null, "Falafel", "Treatment", "Siamese", 4, 2);
 INSERT INTO `Animal` VALUES (null, "Doodles", "Kennel", "Poodle", 3, 1);
 INSERT INTO `Animal` VALUES (null, "Daps", "Kennel", "Boxer", 2, 2);
 
+
 SELECT
     a.id,
     a.name,
     a.breed,
     a.status,
     a.location_id,
-    a.customer_id
-FROM animal a
-WHERE a.id = 3
+    a.customer_id,
+    l.name location_name,
+    l.address location_address,
+	c.name customer_name,
+	c.email customer_email
+FROM Animal a
+JOIN Location l
+    ON l.id = a.location_id
+JOIN Customer c
+	ON c.id = a.customer_id
